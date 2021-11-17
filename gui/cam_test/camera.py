b767c0d08b1ba2820a81cb55b2093cbd6bb2ad12
chickwensrule <80529810+ChickwensRule@users.noreply.github.com>
2021-11-17 17:44:19 +0800

    feat: added working camera test script

--- gui/cam_test/camera.py
+++ gui/cam_test/camera.py
@@ -1,35 +1,42 @@
-from PyQt5.QtWidgets import QApplication, QMainWindow, QWidget#, QVBoxLayout
-from PyQt5.QtMultimedia import QMediaPlayer, QMediaContent
-from PyQt5.QtMultimediaWidgets import QVideoWidget
+from PyQt5.QtWidgets import QApplication, QMainWindow, QWidget, QGridLayout
+from PyQt5.QtMultimedia import QMediaPlayer, QMediaContent, QCameraInfo, QCamera
+from PyQt5.QtMultimediaWidgets import QVideoWidget, QCameraViewfinder
 from PyQt5.QtCore import Qt, QUrl
+import cv2
 import sys
 
-class Camera(QMainWindow):
+class CameraDisplay(QMainWindow):
     def __init__(self):
         super().__init__()
 
-        self.setWindowTitle('Camera Testing')
-        self.setFixedSize(800,400)
+        self.setWindowTitle('Video Testing')
+
+        self.cameras = QCameraInfo.availableCameras()
+        self.viewfinder = QCameraViewfinder()
+
+        self.viewfinder.show()
+        self.choose_camera(0) # add picker
+
+        self.parent_widget = QWidget()
+        self.setCentralWidget(self.parent_widget)
+
+        self.layout = QGridLayout()
+        self.layout.addWidget(self.viewfinder, 0,0,0,0)
+
+        self.parent_widget.setLayout(self.layout)
 
 
-        self.media_player = QMediaPlayer(self, QMediaPlayer.VideoSurface)
-        self.video = QVideoWidget()
 
-
-        self.media_player.setVideoOutput(self.video)
-
-        self.media_player.setMedia(QMediaContent(QUrl.fromLocalFile('/Users/krishnakundalia/mate-2022-azure-1/gui/cam_test/rick_roll.mp4')))
-        self.media_player.play()
-
-        self.setCentralWidget(self.video)
-
-        self.video.show()
+    def choose_camera(self, index):
+        self.camera = QCamera(self.cameras[index])
+        self.camera.setViewfinder(self.viewfinder)
+        self.camera.start()
 
 
 if __name__ == '__main__':
     app = QApplication([])
 
-    window = Camera()
+    window = CameraDisplay()
     window.show()
 
     sys.exit(app.exec())
---
+++ gui/cam_test/video.py
@@ -0,0 +1,36 @@
+from PyQt5.QtWidgets import QApplication, QMainWindow, QWidget#, QVBoxLayout
+from PyQt5.QtMultimedia import QMediaPlayer, QMediaContent
+from PyQt5.QtMultimediaWidgets import QVideoWidget
+from PyQt5.QtCore import Qt, QUrl
+import cv2
+import sys
+
+class Video(QMainWindow):
+    def __init__(self):
+        super().__init__()
+
+        self.setWindowTitle('Video Testing')
+        self.setFixedSize(800,400)
+
+
+        self.media_player = QMediaPlayer(self, QMediaPlayer.VideoSurface)
+        self.video = QVideoWidget()
+
+
+        self.media_player.setVideoOutput(self.video)
+
+        self.media_player.setMedia(QMediaContent(QUrl.fromLocalFile('/Users/krishnakundalia/mate-2022-azure-1/gui/cam_test/rick_roll.mp4')))
+        self.media_player.play()
+
+        self.setCentralWidget(self.video)
+
+        self.video.show()
+
+
+if __name__ == '__main__':
+    app = QApplication([])
+
+    window = Video()
+    window.show()
+
+    sys.exit(app.exec())
