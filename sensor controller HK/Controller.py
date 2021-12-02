1d4b0c09f8d897797651f03b47888928ae89d964
HadiK1205 <91250636+HadiK1205@users.noreply.github.com>
2021-12-02 17:19:39 +0800

    Add files via upload
    This file provides the values from the controller

---
+++ sensor controller HK/Controller.py
@@ -0,0 +1,10 @@
+import serial
+from threading import Thread
+
+class Controller:
+    def __init__(self, control) 
+        packet int[]
+        self.control = serial.Serial()
+        x = self.control.read()
+        packet = x
+    
