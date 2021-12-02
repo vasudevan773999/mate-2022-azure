8406e0d80b3e545f18356b9fd929b3df29fb1832
luke780440 <91250800+luke780440@users.noreply.github.com>
2021-12-02 16:55:15 +0800

    Add files via upload
    Fixed serial interface code

--- interface_library/AzureBot_Test.py
+++ interface_library/AzureBot_Test.py
@@ -4,6 +4,7 @@ from threading import Thread
 import serial
 import controls
 
+
 class Comms:
     def __init__(self, port: int, baud_rate: int):
         self.port = port
@@ -16,11 +17,22 @@ class Comms:
     def run(self):
         while True: 
             packetControls = controls.Controls() #change later
-            
-            
-            packetControls.split(chr())
-            left_bumper = packetControls[1] #and repeat... will finalize once I know what values will come
-            
+            leftJoy_LR = packetControls.packet[0] #and repeat... will finalize once I know what values will come
+            leftJoy_UD = packetControl.packet[1]
+
+            while True:
+                if leftJoy_LR < [43,43]
+                    packet_leftJoy_stopped = chr(1) + chr(5) + chr((leftJoy_speed=0).encode("latin")) + chr(8)
+                    self.ser.write(packet_leftJoy_stopped)
+                else:
+                    packet_leftJoy = chr(1) + chr(5) + chr((leftJoy_speed).encode("latin")) + chr(8) 
+                    self.ser.write(packet_leftJoy)
+
+                #create serial connection computer and mc
+                #read data
+                #test and figure out what each byte means
+                #take data, convert it, and send to systems
+                #[-127,127]
 
     def start_thread(self):
         start_thread = Thread(target = self.run)  
