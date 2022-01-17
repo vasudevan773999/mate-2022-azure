990b337842a347ef600493196c4fd62369369491
vasudevan773999 <90367424+vasudevan773999@users.noreply.github.com>
2022-01-17 14:24:17 +0800

    Update Thruster.ino

--- Thruster/Thruster.ino
+++ Thruster/Thruster.ino
@@ -1,12 +1,26 @@
 #include <Servo.h>
-Servo myservo;
+
+
+Servo myservo_rotate;
+Servo myservo_grab; 
 
 int thruster_Speed = 1500;
-Servo mythruster;
+Servo mythruster_FR;
+Servo mythruster_BR;
+Servo mythruster_BL;
+Servo mythruster_FL;
+Servo mythruster_R;
+Servo mythruster_L;
 int pos = 0;
 void setup() {
-  mythruster.attach(7);
-  myservo.attach(9);  // attaches the servo on pin 9 to the servo object
+  mythruster_FR.attach(1);
+  mythruster_BR.attach(2);
+  mythruster_BL.attach(3);
+  mythruster_FL.attach(4);
+  mythruster_R.attach(5);
+  mythruster_L.attach(6);
+  myservo_rotate.attach(7); 
+  myservo_grab.attach(8);// attaches the servo on pin 9 to the servo object
   mythruster.writeMicroseconds(1500);
   delay(200);
   Serial.begin(9600);
@@ -15,8 +29,8 @@ void setup() {
 }
 
 void loop() {
-  myservo.write(0);              // tell servo to go to position in variable 'pos'
-
+  myservo_grab.write(0);              // tell servo to go to position in variable 'pos'
+  myservo_rotate.write(0);     
 
   Serial.print("Enter thruster speed: ");
   int temp = Serial.parseInt();
@@ -24,10 +38,16 @@ void loop() {
   //changing the fan speed
   Serial.print("thruster_speed = ");
   Serial.println(thruster_Speed);
-  mythruster.writeMicroseconds(thruster_Speed);
+  mythruster_FR.writeMicroseconds(thruster_Speed);
+  mythruster_BR.writeMicroseconds(thruster_Speed);
+  mythruster_BL.writeMicroseconds(thruster_Speed);
+  mythruster_FL.writeMicroseconds(thruster_Speed);
+  mythruster_R.writeMicroseconds(thruster_Speed);
+  mythruster_L.writeMicroseconds(thruster_Speed);
   Serial.write(9600);
   Serial.available();
-  myservo.write(45);              // tell servo to go to position in variable 'pos'
+  myservo_rotate.write(45);  
+  myservo_grab.write(45);// tell servo to go to position in variable 'pos'
   delay(300);
  
 }
