9f08fdbfb392cbd7cf762e46520b747270964c14
vasudevan773999 <90367424+vasudevan773999@users.noreply.github.com>
2022-01-18 16:50:31 +0800

    Update Serial_Interface_Code.ino

--- Serial_Interface_Code/Serial_Interface_Code.ino
+++ Serial_Interface_Code/Serial_Interface_Code.ino
@@ -4,16 +4,8 @@
 
 #include <Servo.h>
 char header = 1
-char FR = 2
-char FL = 3
-char BR = 4
-char BL = 5
-char Right = 6
-char Left = 7
-char Servo_rotate = 8
-char Servo_grab = 9
 char footer = 10
-int motorused
+  
 Servo mythruster_FR;
 Servo mythruster_BR;
 Servo mythruster_BL;
@@ -53,33 +45,38 @@ void setup() {
 void loop() {
 
 
-   if (Serial.available==4); {
+    if (Serial.available>=4){
       byte packetheader = Serial.read();
-   if (packetheader==header);
-      byte motor = Serial.read();
-  
-      motor = incomingByte[2];
-      int incomingByte = Serial.parseInt(); 
+      if (packetheader==header){
+        byte motor = Serial.read();
+        int8_t speed = Serial.read();
+        int motor_speed = (int) (1500 + speed * (500 / 127));
+        if (incomingByte[2] == 2){
+          mythruster_FR.writeMicroseconds(motor_speed);
+        } else if (incomingByte[2] == 3){
+          mythruster_FL.writeMicroseconds(motor_speed);
+        } else if (incomingByte[2] == 4){
+          mythruster_BR.writeMicroseconds(motor_speed);
+        } else if (incomingByte[2] == 5){
+          mythruster_BL.writeMicroseconds(motor_speed);
+        } else if (incomingByte[2] == 6){
+          mythruster_R.writeMicroseconds(motor_speed);
+        } else if (incomingByte[2] == 7){
+          mythruster_L.writeMicroseconds(motor_speed);
+        } else if (incomingByte[2] == 8){
+          myservo_rotate.write(motor_speed);
+        } else if (incomingByte[2] == 9){
+          myservo_grab.write(motor_speed);
+        }
+      }
     }
+            
+      
+    
+           
     
   
-    if (incomingByte[1] == 2){
-       mythruster_FR.writeMicroseconds(motor_speed);
-    } else if (incomingByte[1] == 3){
-      mythruster_FL.writeMicroseconds(motor_speed);
-    } else if (incomingByte[1] == 4){
-      mythruster_BR.writeMicroseconds(motor_speed);
-    } else if (incomingByte[1] == 5){
-      mythruster_BL.writeMicroseconds(motor_speed);
-    } else if (incomingByte[1] == 6){
-      mythruster_R.writeMicroseconds(motor_speed);
-    } else if (incomingByte[1] == 7){
-      mythruster_L.writeMicroseconds(motor_speed);
-    } else if (incomingByte[1] == 8){
-      myservo_rotate.write(motor_speed);
-    } else if (incomingByte[1] == 9){
-      myservo_grab.write(motor_speed);
-    }
+    
 
 
 
