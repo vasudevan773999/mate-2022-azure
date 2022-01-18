90cbad2df5e21a2d1fccdac6b69e159c38e5e066
vasudevan773999 <90367424+vasudevan773999@users.noreply.github.com>
2022-01-18 15:59:20 +0800

    Update Serial_Interface_Code.ino

--- Serial_Interface_Code/Serial_Interface_Code.ino
+++ Serial_Interface_Code/Serial_Interface_Code.ino
@@ -53,21 +53,21 @@ void loop() {
     
   
     if (incomingByte[1] == 2){
-       mythruster_FR.writeMicroseconds((incomingByte[2]));
+       mythruster_FR.writeMicroseconds(motor_speed);
     } else if (incomingByte[1] == 3){
-      mythruster_FL.writeMicroseconds((incomingByte[2]));
+      mythruster_FL.writeMicroseconds(motor_speed);
     } else if (incomingByte[1] == 4){
-      mythruster_BR.writeMicroseconds( (incomingByte[2]));
+      mythruster_BR.writeMicroseconds(motor_speed);
     } else if (incomingByte[1] == 5){
-      mythruster_BL.writeMicroseconds((incomingByte[2]);
+      mythruster_BL.writeMicroseconds(motor_speed);
     } else if (incomingByte[1] == 6){
-      mythruster_R.writeMicroseconds((incomingByte[2]));
+      mythruster_R.writeMicroseconds(motor_speed);
     } else if (incomingByte[1] == 7){
-      mythruster_L.writeMicroseconds( (incomingByte[2]));
+      mythruster_L.writeMicroseconds(motor_speed);
     } else if (incomingByte[1] == 8){
-      myservo_rotate.write((incomingByte[2]));
+      myservo_rotate.write(motor_speed);
     } else if (incomingByte[1] == 9){
-      myservo_grab.write( (incomingByte[2]);
+      myservo_grab.write(motor_speed);
     }
 
 
