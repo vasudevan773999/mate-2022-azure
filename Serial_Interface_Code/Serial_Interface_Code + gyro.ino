b9192de87095cb36b2e64f599a7221444deacfbb
vasudevan773999 <90367424+vasudevan773999@users.noreply.github.com>
2022-03-16 11:20:26 +0800

    Update Serial_Interface_Code + gyro.ino

--- Serial_Interface_Code/Serial_Interface_Code + gyro.ino
+++ Serial_Interface_Code/Serial_Interface_Code + gyro.ino
@@ -1,89 +1,125 @@
 
-import serial
-#include <Wire.h>
-#include <Adafruit_Sensor.h>
-#include <Adafruit_BNO055.h>
+
 #include <utility/imumaths.h>
-#include <Servo.h>
+Adafruit_BNO055 bno = Adafruit_BNO055(55);
+
+Servo motFR;
+Servo motBR;
+Servo motBL;
+Servo motFL;
+Servo motR;
+Servo motL;
+Servo claw_rotate;
+Servo claw_grab;
+
 char header = 1
 char footer = 10
 
-  
-Servo mythruster_FR;
-Servo mythruster_BR;
-Servo mythruster_BL;
-Servo mythruster_FL;
-Servo mythruster_R;
-Servo mythruster_L;
-Servo myservo_rotate;
-Servo myservo_grab;  
-
-
 void setup() {
-  myservo_rotate.attach(13);
-  myservo_grab.attach(14);
-  
-  mythruster_FR.attach(7,1000,2000);
-  mythruster_FR.writeMicroseconds(1500);
-  
-  mythruster_BR.attach(8,1000,2000);
-  mythruster_BR.writeMicroseconds(1500);
-  
-  mythruster_BL.attach(9,1000,2000);
-  mythruster_BL.writeMicroseconds(1500);
-  
-  mythruster_FL.attach(10,1000,2000);
-  mythruster_FL.writeMicroseconds(1500);
-  
-  mythruster_R.attach(11,1000,2000);
-  mythruster_R.writeMicroseconds(1500);
-  
-  mythruster_L.attach(12,1000,2000);
-  mythruster_L.writeMicroseconds(1500);
-  
-  delay(200);
   Serial.begin(9600);
+  
+  motFR.attach(A5, 1000, 2000);
+  motBR.attach(A2, 1000, 2000);
+  motBL.attach(A4, 1000, 2000);
+  motFL.attach(A3, 1000, 2000);
+  motR.attach(10,1000, 2000);
+  motL.attach(9, 1000, 2000);
+  claw_rotate.attach(11,0,120);
+  claw_grab.attach(12,0,120);
+
+  motFR.writeMicroseconds(1500);
+  motBR.writeMicroseconds(1500);
+  motBL.writeMicroseconds(1500);
+  motFL.writeMicroseconds(1500);
+  motR.writeMicroseconds(1500);
+  motL.writeMicroseconds(1500);
+
+  delayMicroseconds(5000);
+  while (!Serial);
 }
 
 void loop() {
-
-    if (Serial.available>=4){
-      byte packetheader = Serial.read();
-      if (packetheader==header){
-        byte motor = Serial.read();
-        int8_t speed = Serial.read();
-        int motor_speed = (int) (1500 + speed * (500 / 127));
-        byte motor_servo = serial.read();
-        int8_t servo_speed = Serial.read();
-        int servo_speed_convert = (int)((servo_speed * 0.73)-255);
+  if(Serial.available() >= 1){
+    //byte header = Serial.parseInt();
+    //if (header == 1){
+    int val = Serial.parseInt();
+      int motor = val / 1000;
+      int m = val % 1000;
+      if (m == 0){
+        m = 1500;
+      }
+      else if (m < 0 || m > 127){ 
+        if (m > 127) { m -= 0xFF; }
+        m = map(m, -127, -1, 1000, 1499);
+      }
+      else if (m > 0){
+        m = map(m, 1, 127, 1501, 2000);
+      }
+      Serial.println(m);
+      if (motor == 2){
+        Serial.print("Motor FR");
+        motFR.writeMicroseconds(m);
+      } 
+      else if (motor == 3){
+        Serial.print("Motor FL");
+        motFL.writeMicroseconds(m);
+      } 
+      else if (motor == 4){
+        Serial.print("Motor BR");
+        motBR.writeMicroseconds(m);
+      } 
+      else if (motor == 5){
+        Serial.print("Motor BL");
+        motBL.writeMicroseconds(m);
+      } 
+      else if (motor == 6){
+        Serial.print("Motor R");
+        motR.writeMicroseconds(m);
+      } 
+      else if (motor == 7){
+        Serial.print("Motor L");
+        motL.writeMicroseconds(m);
+      }
+      else if (motor==9){
+        claw_grab.write(m);
+      }
+      else if (motor == 14){
+          motFR.writeMicroseconds(1500);
+          motFL.writeMicroseconds(1500);
+          motBR.writeMicroseconds(1500);
+          motBL.writeMicroseconds(1500);
+          motR.writeMicroseconds(1500);
+          motL.writeMicroseconds(1500);
         
-        if (incomingByte[2] == 2){
-          mythruster_FR.writeMicroseconds(motor_speed);
-        } else if (incomingByte[2] == 3){
-            mythruster_FL.writeMicroseconds(motor_speed);
-        } else if (incomingByte[2] == 4){
-            mythruster_BR.writeMicroseconds(motor_speed);
-        } else if (incomingByte[2] == 5){
-            mythruster_BL.writeMicroseconds(motor_speed);
-        } else if (incomingByte[2] == 6){
-            mythruster_R.writeMicroseconds(motor_speed);
-        } else if (incomingByte[2] == 7){
-            mythruster_L.writeMicroseconds(motor_speed);
-        } else if (incomingByte[2] == 8){
-            myservo_rotate.write(servo_speed_convert);
-        } else if (incomingByte[2] == 9){
-            myservo_grab.write(servo_speed_convert);
+      }
+      else if (motor == 13){
+        if (m == 127){
+          Serial.print("Top Motor Down");
+          motFR.writeMicroseconds(1700);
+          motFL.writeMicroseconds(1700);
+          motBR.writeMicroseconds(1700);
+          motBL.writeMicroseconds(1700);
         }
+        else if (m == 254){
+          Serial.print("Top Motor Up");
+          motFR.writeMicroseconds(1300);
+          motFL.writeMicroseconds(1300);
+          motBR.writeMicroseconds(1300);
+          motBL.writeMicroseconds(1300);
         }
       }
-    }
-
-
-   reportIMUData();
+  }
+}
+    header_gyro = header_control[0]
+    orientation = orientation_control[1]
+    rotation_vector = rotation_vector_control[2]
+    linear_acceleration = linear_acceleration_control[3]
+    footer_gyro = footer_control[4]
+    reportIMUData();
 }
 
 void reportIMUData() {
-    sensors_event_t orientation , gyro , accel;
+    sensors_event_t orientation , rotation_vector , linear_acceleration;
     bno.getEvent(&orientation, Adafruit_BNO055::VECTOR_EULER);
     bno.getEvent(&gyro, Adafruit_BNO055::VECTOR_GYROSCOPE);
     bno.getEvent(&accel, Adafruit_BNO055::VECTOR_LINEARACCEL);
