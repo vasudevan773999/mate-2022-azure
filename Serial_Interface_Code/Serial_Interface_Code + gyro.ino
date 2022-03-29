29ff4fe9f4d2c499305d32206ab6a91d4bd52952
vasudevan773999 <90367424+vasudevan773999@users.noreply.github.com>
2022-03-29 17:47:05 +0800

    Update Serial_Interface_Code + gyro.ino

--- Serial_Interface_Code/Serial_Interface_Code + gyro.ino
+++ Serial_Interface_Code/Serial_Interface_Code + gyro.ino
@@ -93,7 +93,7 @@ void loop() {
           motBL.writeMicroseconds(1500);
           motR.writeMicroseconds(1500);
           motL.writeMicroseconds(1500);
-          claw_grav.write(9000);
+          claw_grab.write(9000);
         
       }
       else if (motor == 13){
